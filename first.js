03b44fcf9125d4240e060f97524251cca9bbc1a0
Kaushal_Sharma <128350300+Kaushalsharm@users.noreply.github.com>
2024-06-29 08:23:10 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,3 +1,35 @@
+const accessKey = 'uAAemp4ID-Yv0ucsxIO7N5SK72NIff2PqH2A32TZYqc';
+const query = 'education,books,laptop'; // Multiple terms separated by comma
+const endpoint = `https://api.unsplash.com/photos/random?query=${query}&client_id=${accessKey}`;
+
+// Select the <img> elements
+const imgElements = [
+    document.getElementById('randomImage1'),
+    document.getElementById('randomImage2'),
+    document.getElementById('randomImage3')
+];
+
+imgElements.forEach(imgElement => {
+    fetch(endpoint)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Assuming data is an object with the random photo details
+            const randomPhotoUrl = data.urls.regular;
+            imgElement.src = randomPhotoUrl;
+            imgElement.alt = data.alt_description; // Set alt description dynamically
+        })
+        .catch(error => {
+            console.error('Error fetching random image:', error);
+        });
+});
+
+
+
 burger=document.querySelector('.burger')
 navbar=document.querySelector('.navbar')
 navlist=document.querySelector('.nav-list')
@@ -8,3 +40,14 @@ burger.addEventListener('click',()=>{
     navlist.classList.toggle('v-class-res');
     navbar.classList.toggle('h-nav-res');
 })
+
+// const images= document.querySelector(".imgfluid");
+// const URL = "https://api.unsplash.com/photos/?client_id=uAAemp4ID-Yv0ucsxIO7N5SK72NIff2PqH2A32TZYqc";
+
+// fetch(URL)
+// .then((res)=>{
+//     return res.json()
+// })
+// .then((data)=>{
+//     this.images
+// })
